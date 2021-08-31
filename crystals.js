29cbba3dd9835c262c932ecc254a77e610cb3267
Phil Zona <phil.b.zona@gmail.com>
2021-08-31 08:25:18 -0400

    Add stepped lines

--- crystals.js
+++ crystals.js
@@ -20,6 +20,12 @@ function draw() {
 }
 
 function simpleLines() {
+  const stepsOut = 8;
+  const numSteps = randomSelectTwo() ? stepsOut : int(stepsOut * 1.25);
+  const step = (CRYSTAL_SIZE / 2) / numSteps;
+  const start = floor(random(0, numSteps));
+  const stop = floor(random(start + 1, numSteps + 1));
+
   const numShapes = randomSelectTwo() ? SIDES : SIDES * 2;
   const angle = 360 / numShapes;
   
@@ -32,7 +38,7 @@ function simpleLines() {
   push();
     translate(width/2, height/2);
     for (let i = 0; i < numShapes; i++) {
-      line(0, 0, 0, CRYSTAL_SIZE / 2);
+      line(start * step, 0, stop * step, 0);
       rotate(angle);
     }
   pop();
