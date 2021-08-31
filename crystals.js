e063e84af9e207b1e5a5a7eb45850c7694b3433d
Phil Zona <phil.b.zona@gmail.com>
2021-08-31 08:13:21 -0400

    Rebuild lines function

--- crystals.js
+++ crystals.js
@@ -15,8 +15,27 @@ function setup() {
 }
 
 function draw() {
-  // testLines();
   outlineShape();
+  simpleLines();
+}
+
+function simpleLines() {
+  const numShapes = randomSelectTwo() ? SIDES : SIDES * 2;
+  const angle = 360 / numShapes;
+  
+  const strokeColor = getRandomFromPalette();
+  const weight = randomSelectTwo() ? 1 : 3;
+
+  noFill();
+  stroke(strokeColor);
+  strokeWeight(weight);
+  push();
+    translate(width/2, height/2);
+    for (let i = 0; i < numShapes; i++) {
+      line(0, 0, 0, CRYSTAL_SIZE / 2);
+      rotate(angle);
+    }
+  pop();
 }
 
 function outlineShape() {
@@ -32,27 +51,6 @@ function outlineShape() {
       ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
     } else {
       hexagon(0, 0, CRYSTAL_SIZE / 2);
-    }
-  pop();
-}
-
-function testLines() {
-  let numShapes = randomSelectTwo() ? SIDES : SIDES * 2;
-  let strokeColor = getRandomFromPalette();
-
-  noFill();
-
-  push();
-    translate(width/2, height/2);
-    stroke(PALETTE[0]);
-    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
-
-    for (let i = 0; i < numShapes; i++) {
-      const angle = 360 / numShapes;
-
-      stroke(strokeColor);
-      line(0, 0, 0, CRYSTAL_SIZE / 2);
-      rotate(angle);
     }
   pop();
 }
